8e0ede867955cad510ef389c11e67606b24aab47
bitcamp\BIT <0215bitcamp@gmail.com>
2024-09-27 16:46:59 +0900

    CI/CD test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Home = () => {
+    useEffect(()=>{
+        alert("hi");
+    },[])
     return (
         <>
             <div>
